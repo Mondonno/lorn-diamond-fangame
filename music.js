75e578373e5c9be611655b238200ae939c1c4f8e
Mondonno <mikolaj.mocek@icloud.com>
2020-07-18 12:45:10 +0200

    Init on update

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
 
-    var audioPlayer = new Audio('./music/diamond-music.mp3');
+    var audioPlayer = new Audio('https://cdn.discordapp.com/attachments/688391374252670986/733997610024697876/1_diamond-music.mp3');
     audioPlayer.volume = 0.4;
 audioPlayer.play();
